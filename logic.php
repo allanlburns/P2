8b66036135ab818eb13f9aab4cacd7cf2c51dc43
Allan Burns <allanlburns@yahoo.com>
2014-10-02 13:25:26 -0400

    Is this actually how I'm going to do this? Will this work? in logic.php.

--- logic.php
+++ logic.php
@@ -1,5 +1,26 @@
-$world_list[0] = 'Jefferson';
-$world_list[1] = 'Lincoln';
-$world_list[2] = 'Taft';
-$world_list[3] = 'Kennedy';
-$world_list[4] = 'Nixon';
+<?php
+
+$world_list_president[0] = 'Jefferson';
+$world_list_president[1] = 'Lincoln';
+$world_list_president[2] = 'Taft';
+$world_list_president[3] = 'Kennedy';
+$world_list_president[4] = 'Nixon';
+
+$world_list_color[0] = blue;
+$world_list_color[1] = red;
+$world_list_color[2] = yellow;
+$world_list_color[3] = green;
+$world_list_color[4] = black;
+$world_list_color[5] = white;
+$world_list_color[6] = purple
+
+$wordone = array_rand($world_list_president);
+$wordtwo = array_rand($world_list_color);
+$wordthree= array_rand($wordlist_day);
+$wordfour = array_rand($worldlist_greek);
+
+echo $wordone . $wordtwo . 
+
+
+?>
+
