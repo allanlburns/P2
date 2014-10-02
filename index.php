1f37b0a26aaebf12975204e3203c25ff22a459aa
Allan Burns <allanlburns@yahoo.com>
2014-10-02 14:16:42 -0400

    added foreach loop to index.php, but it didn't help

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE: html>
 <html>
 	<head>
-		<?php require logic.php; ?>
-		
+		<?php require 'logic.php'; ?>
+
 		<link type="text/css" rel= "stylesheet" href="stylesheet.css" 
 	
 		<title><index.php></title>
@@ -12,6 +12,15 @@
 		<h1>Password Generator!</h1>
 
 		<p>Create your own xkcd-style password</p>
+
+		<?php 
+
+			foreach($wordlist_president as $wordone) {
+
+			echo $wordone . $wordtwo; 
+		}
+
+		?>
 
 
 
