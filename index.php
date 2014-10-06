b220a236581453fede54c880b02076329ebdfe8f
Allan Burns <allanlburns@yahoo.com>
2014-10-05 22:40:33 -0400

    Added functional for loop, but only prints out array position.

--- index.php
+++ index.php
@@ -17,12 +17,10 @@
 
 		<p>Number of words></p><br>
 
-		<input type ='number' name='numberofwords' min= '4'
+		<input type ='number' name='$length' min= '4'
 			max='8' step='1' value='4'><br>
 
-		<!--input type="number" name="age" id="age"
-                min="18" max="120" step="1" value="18"> -->
-
+		
 		<p>Use a symbol?></p><br>
 
 		<input type ='checkbox' name='symbol' value='yes'><br>
@@ -30,25 +28,30 @@
 		<p>Include a number?></p><br>
 
 		<input type ='checkbox' name='number' value='yes'><br>
-
-
 		
 		<input type ='submit' value='Create your password!'>
 
+		
 		<pre><?php print_r($_GET); ?></pre>
-
 
 		Your password is:
 
-		<?php echo $_GET['numberofwords']; ?><br>
+		<?php echo $password ?>
 
-		<?php if (isset($var)) {
+		
 
-		 echo $_GET['symbol']; }?><br>
+		
+
+	<?php echo $_GET['$length']; ?>
+
+	<?php if (isset($var)) {
+
+		 	echo $_GET['symbol']; }?>
 
 		<?php if (isset($var2)) {
 
-			echo $_GET['number']; }?><br>
+			echo $_GET['number']; }?>
+
 
 
 
--- logic.php
+++ logic.php
@@ -1,13 +1,53 @@
 <?php
 
-$worldlist = array('Jefferson', 'Lincoln', 'Taft', 'Kennedy', 'Nixon', 
+$wordlist = array('Jefferson', 'Lincoln', 'Taft', 'Kennedy', 'Nixon', 
 'blue', 'red', 'yellow', 'green', 'black', 'white', 'purple', 'Sunday', 'Monday', 'Tuesday',
 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'lamda', 'iota', 'rho', 'sigma', 'alpha',
-'beta', 'gamma')
+'beta', 'gamma');
 
-//for($i = 0; $i < 'numberofwords'; $i++;)
 
-//$password = array()
+
+$password = array_rand ($wordlist);
+
+for($i=0; $i < 8; $i++) {
+
+	echo array_rand ($wordlist) . $password;
+
+}
+
+
+
+
+
+
+
+
+
+
+//$symbols = array('!', '@', '%', '&', '*', '+', '=');
+
+
+	
+	
+
+
+
+
+
+
+
+/*
+
+'numberofwords' = $length;
+
+//for($i = 0; $i < 'length'; $i++;) {
+
+	//array(rand)[wordlist];
+}
+
+*/
+
+//$password = array();
 
 
 
