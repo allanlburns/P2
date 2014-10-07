6b444a0264db88b30d84be4f8f020063c7ef6551
Allan Burns <allanlburns@yahoo.com>
2014-10-07 14:23:48 -0400

    added '[space]/' to form tags.

--- index.php
+++ index.php
@@ -13,23 +13,23 @@
 
 		<h2>Create your own xkcd-style password</h2>
 
-		<form action='logic.php' method='GET'>
+		<form action='index.php' method='POST'>
 
 		<p>Number of words></p><br>
 
-		<input type ='number' name='$length' min= '4'
+		<input type ='number' name='length' min= '4'
 			max='8' step='1' value='4'><br>
 
 		
 		<p>Use a symbol?</p><br>
 
-		<input type ='checkbox' name='symbol' value='yes'><br>
+		<input type ='checkbox' name='symbol' value='yes' /><br>
 
 		<p>Include a number?</p><br>
 
-		<input type ='checkbox' name='number' value='yes'><br><br>
+		<input type ='checkbox' name='number' value='yes' /><br><br>
 		
-		<input type ='submit' value='Create your password!'><br><br>
+		<input type ='submit' value='Create your password!' /><br><br>
 
 		
 		Your password is:
@@ -37,9 +37,11 @@
 		
 		<?php foreach ($password as $value) {
 
-		echo $wordlist[$value];
+		echo $wordlist[$value]; 
 
 		}
+
+
 
 		?>
 
